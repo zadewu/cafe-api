082723f298fa1afc9e8cec3a54f12cdbeac9e5da
Huy Phan <pbhuy93@gmail.com>
2023-09-08 00:30:46 +0700

    * Enhance api error

--- src/main/java/vn/cmax/cafe/security/DefaultUserDetailService.java
+++ src/main/java/vn/cmax/cafe/security/DefaultUserDetailService.java
@@ -2,6 +2,7 @@ package vn.cmax.cafe.security;
 
 import java.util.stream.Collectors;
 import org.springframework.security.core.userdetails.*;
+import vn.cmax.cafe.exception.CmaxException;
 import vn.cmax.cafe.user.UserEntity;
 import vn.cmax.cafe.user.UserService;
 
@@ -14,22 +15,30 @@ public class DefaultUserDetailService implements UserDetailsService, UserDetails
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    UserEntity userEntity = this.userService.getUser(username);
-    return User.builder()
-        .username(userEntity.getUsername())
-        .password(userEntity.getPassword())
-        .roles(
-            userEntity.getRoles().stream()
-                .map(role -> role.name())
-                .collect(Collectors.toList())
-                .toArray(new String[0]))
-        .build();
+    try {
+      UserEntity userEntity = this.userService.getUser(username);
+      return User.builder()
+          .username(userEntity.getUsername())
+          .password(userEntity.getPassword())
+          .roles(
+              userEntity.getRoles().stream()
+                  .map(role -> role.name())
+                  .collect(Collectors.toList())
+                  .toArray(new String[0]))
+          .build();
+    } catch (CmaxException ex) {
+      throw new UsernameNotFoundException(ex.getMessage());
+    }
   }
 
   @Override
   public UserDetails updatePassword(UserDetails user, String newPassword) {
-    UserEntity userEntity = this.userService.updateUserPassword(user.getUsername(), newPassword);
-    return withNewPassword(userEntity, newPassword);
+    try {
+      UserEntity userEntity = this.userService.updateUserPassword(user.getUsername(), newPassword);
+      return withNewPassword(userEntity, newPassword);
+    } catch (CmaxException ex) {
+      throw new RuntimeException(ex.getMessage());
+    }
   }
 
   private UserDetails withNewPassword(UserEntity userEntity, String newPassword) {
